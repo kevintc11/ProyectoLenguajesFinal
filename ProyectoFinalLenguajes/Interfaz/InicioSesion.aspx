3a7ee78e307d4a9867850dcb445e71186fef7392
Juan Carlos Valverde Araya <juan.valverde@ucrso.info>
2020-06-22 20:47:34 -0600

    Inicio1

--- ProyectoFinalLenguajes/Interfaz/InicioSesion.aspx
+++ ProyectoFinalLenguajes/Interfaz/InicioSesion.aspx
@@ -6,10 +6,29 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <style type="text/css">
+        #Password1 {
+            width: 180px;
+        }
+        #iptPassword {
+            width: 180px;
+        }
+    </style>
 </head>
-<body>
+<body style="text-align: center">
     <form id="form1" runat="server">
         <div>
+            Bienvenidos<br />
+            <br />
+            Usuario<br />
+            <asp:TextBox ID="TextBox2" runat="server" Width="180px"></asp:TextBox>
+            <br />
+            Contraseña<br />
+            <input id="iptPassword" type="password" /><br />
+            <br />
+            <asp:Button ID="btSign" runat="server" Text="Iniciar Sesion" />
+&nbsp;O
+            <asp:Button ID="btRegist" runat="server" Text="Registrarse" />
         </div>
     </form>
 </body>
--- ProyectoFinalLenguajes/Interfaz/InicioSesion.aspx.designer.cs
+++ ProyectoFinalLenguajes/Interfaz/InicioSesion.aspx.designer.cs
@@ -22,5 +22,32 @@ namespace Interfaz
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control btSign.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btSign;
+
+        /// <summary>
+        /// Control btRegist.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btRegist;
     }
 }
